2af6f691bae4eca4b7ff63610d689b7f062c23f6
msn.dproyect <msn.dproyect@gmail.com>
2025-05-04 22:01:57 -0500

    se actualizo ahora se instalo los iconos en vez de usar link, tambien se creo carpetas theme y algunos archivos para configurar la plantlla y darle funcionalidad

---
+++ src/theme/purpleTheme.js
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material";
+import { red } from "@mui/material/colors";
+
+export const purpleTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#673ab7",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: "#121212",
+      paper: "#121212",
+    },
+  },
+});
